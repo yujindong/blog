7471b16384483c693732d09799fd2bd758de11a2
yujindong <yujindong1985@gmail.com>
2025-07-24 18:16:58 +0800

    init

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -1,6 +1,6 @@
-import { openapi } from "@/lib/source";
+// import { openapi } from "@/lib/source";
 
-export const { GET, HEAD, PUT, POST, PATCH, DELETE } = openapi.createProxy({
-  // optional, we recommend to set a list of allowed origins for proxied requests
-  // allowedOrigins: ["https://example.com"],
-});
+// export const { GET, HEAD, PUT, POST, PATCH, DELETE } = openapi.createProxy({
+//   // optional, we recommend to set a list of allowed origins for proxied requests
+//   // allowedOrigins: ["https://example.com"],
+// });

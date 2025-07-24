88bd1897a6e2d53747c384f9b1ff9fdb2e96e39d
yujindong <yujindong1985@gmail.com>
2025-07-24 18:15:09 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   serverExternalPackages: ["oxc-transform"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
+    ignoreBuildErrors: true,
+  },
   // async redirects() {
   //   return [
   //     {

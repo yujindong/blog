83dc1977389d6c4c00b54e6dcd9e2dbee3c61718
yujindong <yujindong1985@gmail.com>
2025-07-25 18:36:22 +0800

    init

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -1,6 +1,13 @@
+import z from "zod";
+
 type Props = {};
 
 const Home = (props: Props) => {
+  const Player = z.object({
+    username: z.string(),
+    xp: z.number(),
+  });
+  console.log(Player);
   return <div>Home</div>;
 };
 
